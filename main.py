b433441303f008ce25031bf8eecd75584c7d920d
Onkar Ingale <ingale2273@gmail.com>
2021-12-08 19:00:47 +0530

    Updated endpoints
    api.covid19india.org has been deprecated

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import http.client
 import json
 import objectpath
 import mimetypes
-conn = http.client.HTTPSConnection("api.covid19india.org")
+conn = http.client.HTTPSConnection("data.covid19india.org")
 payload = ''
 headers = {}
-conn.request("GET", "/state_district_wise.json", payload, headers)
+conn.request("GET", "/v4/min/data.min.json", payload, headers)
 res = conn.getresponse()
 data = res.read()
 #print(data.decode("utf-8"))
 data = json.loads(data.decode())
-jsonnn_tree = objectpath.Tree(data['Maharashtra'])
+jsonnn_tree = objectpath.Tree(data['MH'])
 result_tuple = tuple(jsonnn_tree.execute('$..Solapur'))
 print(result_tuple)
